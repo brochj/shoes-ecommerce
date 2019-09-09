be45527f4f1dfbbe73913451c9b0833c66991533
Oscar Broch <brochj@gmail.com>
2019-09-09 10:29:12 -0300

    Reactotron + Saga

--- src/config/ReactotronConfig.js
+++ src/config/ReactotronConfig.js
@@ -1,10 +1,12 @@
 import Reactotron from 'reactotron-react-js';
 import { reactotronRedux } from 'reactotron-redux';
+import reactotronSaga from 'reactotron-redux-saga';
 
 // Quando utiliza-se o create react app, ele adiciona essa variavel NODE_ENV quando inicia um `yarn start`
 if (process.env.NODE_ENV === 'development') {
   const tron = Reactotron.configure()
     .use(reactotronRedux())
+    .use(reactotronSaga())
     .connect();
 
   tron.clear();
